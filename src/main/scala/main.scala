47ef3ef360b794a49bfc1f94431a509da8ed008b
zmletters <zemingletters@gmail.com>
2024-11-16 18:13:42 +0800

    20241116Q1_toArrHospital

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,8 +1,25 @@
 // https://github.com/MoH-Malaysia/covid19-public
-// test
-import scala.io.Source
 
-@main def main(): Unit = 
+import scala.io.Source
+import scala.collection.mutable.ArrayBuffer
+
+@main def main(): Unit =
+    val filepath = "src/main/resources/hospital.csv"
+    val file = Source.fromFile(filepath)
+    var count = 0
+    val arrHospital = ArrayBuffer[String]()
+    for (line <- file.getLines()) {
+        arrHospital += line
+    }
+
+    println(arrHospital.mkString("\n"))
+    println()
+    println(arrHospital(1))
+
+    file.close()
+
+
+def mymethod(): Unit =
     val filepath: String = "src/main/resources/hospital.csv"
     val delimiter = ","
     val file = Source.fromFile(filepath)
