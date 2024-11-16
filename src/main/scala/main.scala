3e7c72e434734bbce4e5f494324f7b3ee25f5e4b
zmletters <zemingletters@gmail.com>
2024-11-16 20:36:13 +0800

    20241116Q1_finish

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -4,25 +4,9 @@ import scala.io.Source
 import scala.collection.mutable.*
 
 @main def main(): Unit =
-    // Which state has the highest total hospital bed ? 
-    //arrayMethod()
+    // Which state has the highest total hospital bed?
     listMethod()
 end main
-
-def arrayMethod(): Unit =
-
-    val filepath = "src/main/resources/hospital.csv"
-    val file = Source.fromFile(filepath)
-
-    val arrHospital = ArrayBuffer[String]()
-    for (line <- file.getLines()) {
-        arrHospital += line
-    }
-    var count = arrHospital.length
-    file.close()
-
-    //println(arrHospital.mkString("\n")) // Print all data
-    //println(arrHospital(0)) // Header
 
 def listMethod(): Unit =
     val filepath = "src/main/resources/hospital.csv"
@@ -34,30 +18,20 @@ def listMethod(): Unit =
         listHospital += line.split(",").toList
     }
 
-    println(listHospital.head)
-    println(listHospital(1))
     val header = listHospital.head
     val stateIndex = header.indexOf("state")
-    println(stateIndex)
 
-    val totalBedsByState = ListBuffer[List[String]]()
-    var total = 0
-    var previousRow = List[String]()
-    var hospitalData = listHospital.tail
-    hospitalData = hospitalData.sortBy(row => row(stateIndex))(Ordering[String].reverse)
-    for (row <- hospitalData) {
-        var state = row(stateIndex)
+    val sortedList = listHospital.tail.sortBy(row => row(stateIndex)).reverse
 
-        if (hospitalData.indexOf(row) == 0) {
-            println(row(stateIndex) + ": ")
-            total += row(2).toInt
-        } else if (previousRow(stateIndex) == row(stateIndex)) {
-            total += row(2).toInt
-        } else {
-            println(total)
-            println(row(stateIndex) + ": ")
-            total = 0
-        }
-        previousRow = row
-    }
+    val latestDate = sortedList.head.head
+    println(sortedList.head)
+    println(latestDate)
+
+    var latestList = sortedList.filter(row => row.head == latestDate)
+    println(latestList.length)
+
+    latestList = latestList.sortBy(row => row(2).toInt).reverse
+    println("State with highest hospital bed: ")
+    println(latestList.head(1) + ": " + latestList.head(2))
+
 
