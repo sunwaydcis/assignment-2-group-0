25605ceaa3ce9e229e6c7ee56d6402738d1a04f8
dashlsl <dashaynelumsl@gmail.com>
2024-11-26 11:08:51 +0800

    Added question 2
    Refined it from `main` file and added comments

--- src/main/scala/test1.scala
+++ src/main/scala/test1.scala
@@ -95,20 +95,57 @@ case class HospitalDataHeader(
       hospNonCovid = columns(headerIndex.hospNonCovid).toInt
     )
 
-  /** OUTPUT **/
+  // Find the latest date in the dataset
+  val latestDate = listHospital.maxBy(_.date).date
+  val latestDateList = listHospital.filter(_.date == latestDate)
+
+  /** QUESTION 1 **/
+  // Question 1: Which state has the highest total hospital beds?
+  println("-- Question 1 --")
   // Check if list is empty before proceeding
   if listHospital.nonEmpty then
-    // Find the latest date in the dataset
-    val latestDate = listHospital.maxBy(_.date).date
-
     // Find the state with the maximum beds on the latest date
-    val stateWithMaxBeds = listHospital
-      .filter(_.date == latestDate)
+    val stateWithMaxBeds = latestDateList
       .maxByOption(_.beds) // Use `maxByOption` for safety in case of an empty list
 
     // Output
     stateWithMaxBeds match
-      case Some(state) => println(s"State with max beds on $latestDate: $state")
-      case None => println(s"No data available for the latest date: $latestDate")
+      case Some(state) =>
+        println(s"State with highest total hospital beds is ${state.state} with ${state.beds} beds")
+      case None =>
+        println(s"No data available for the latest date: $latestDate")
   else
     println("No valid hospital data found in the file.")
+
+  /** QUESTION 2 **/
+  // Question 2: Ratio of beds dedicated for COVID-19 to total hospital beds
+  println("\n-- Question 2 --")
+  if listHospital.nonEmpty then
+    // Initialize variables
+    var totalBeds = 0
+    var totalCovidBeds = 0
+
+    // Calculate total beds and dedicated COVID beds
+    for data <- latestDateList do
+      totalBeds += data.beds
+      totalCovidBeds += data.bedsCovid
+
+//    val (totalAvailableBeds, totalDedicatedCOVIDBeds) = latestDateList.foldLeft((0, 0)) {
+//      case ((totalBeds, covidBeds), data) =>
+//        (totalBeds + data.beds, covidBeds + data.bedsCovid)
+//    }
+
+    if totalBeds > 0 then
+      // Calculate ratio
+      val ratio = totalCovidBeds.toDouble / totalBeds.toDouble
+      // Output
+      println(s"Total beds: $totalBeds")
+      println(s"Total beds dedicated to COVID-19: $totalCovidBeds")
+      println(f"Ratio of COVID-19 dedicated beds to total beds: $ratio%.4f")
+    else
+      println("No valid data available to calculate the ratio.")
+  else
+    println("No valid hospital data found in the file.")
+
+  /** QUESTION 3 **/
+  // Question 3: Averages of individuals in each category (suspected/probable, covid, and non-covid) admitted per state
